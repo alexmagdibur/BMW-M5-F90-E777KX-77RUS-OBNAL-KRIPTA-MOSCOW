912400540f0310115143ad9fbf748105df4ffbc4
Alexander Magdibur <dezzzert.official@gmail.com>
2026-04-04 04:46:24 +0300

    junit configured

---
+++ src/test/SimpleTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SimpleTest {
+
+    @Test
+    void junitWorks() {
+        assertEquals(2, 1 + 1);
+    }
+}
